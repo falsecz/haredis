8ac0103884af0658eae37ce881c6cf77482f37b1
Taha Jahangir <mtjahangir@gmail.com>
2014-09-19 18:04:17 +0430

    Disable old master when switching master
    When failover process started manually, we only receive switch-master event.

--- notification.sh
+++ notification.sh
@@ -9,6 +9,7 @@ call_curl () {
 	DATA=`echo "s=$1&action=$2&b=%234" | sed -e s/:/%3A/`
 	curl --silent -o /dev/null $HAPROXY --data "$DATA"
 	echo curl $HAPROXY --data "$DATA"
+	return 0
 }
 
 
@@ -19,7 +20,8 @@ call_curl () {
 	call_curl `echo $ARGS | awk '{print $6 ":" $3 ":" $4}'` 'disable'
 
 [ "$CMD" = "+switch-master" ] && \
-	call_curl `echo $ARGS | awk '{print $1 ":" $4 ":" $5}'` 'enable'
+	call_curl `echo $ARGS | awk '{print $1 ":" $4 ":" $5}'` 'enable' &&
+	call_curl `echo $ARGS | awk '{print $1 ":" $2 ":" $3}'` 'disable'
 
 [ "$CMD" = "-odown" ] && [ "$ARG1" = "master" ] && \
 	call_curl `echo $ARGS | awk '{print $2 ":" $3 ":" $4}'` 'enable'
